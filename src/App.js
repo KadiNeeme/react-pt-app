1072226a03fc3da9c57aa14a68c3cb5a3af939d5
KadiNeeme <kadi.neeme@gmail.com>
2018-11-02 09:05:03 +0200

    Added application files.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Customers from './components/Customers';
+import Trainings from './components/Trainings';
+import Calendar from './components/Calendar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <img src={logo} className="App-logo" alt="logo" />
+          <span className="App-title">Personal Trainer database</span>
         </header>
+         
+        <BrowserRouter baseline>
+          <div>
+             <Navbar />
+        
+
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/customers" component={Customers}/>
+              <Route path="/trainings" component={Trainings}/>
+              <Route path="/calendar" component={Calendar}/>
+            </Switch>
+
+          </div>
+        </BrowserRouter>
+
       </div>
     );
   }
